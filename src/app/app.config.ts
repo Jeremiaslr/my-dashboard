a43bf55827dce6bb24015d2e451d79780b926bef
jere <jere77lagos@gmail.com>
2024-08-29 18:08:02 +0200

    implement User list component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,10 @@ export const appConfig: ApplicationConfig = {
         //   console.log({ transitionInfo })
         // }
       }),
+    ),
+
+    importProvidersFrom(
+      HttpClientModule,
     )
   ]
 };
--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { TitleComponent } from '@shared/title/title.component';
+// import { User } from '@interfaces/req-response';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+
+
+    @if( user() ) {
+
+    <app-title [title]="titleLabel()" />
+    <section>
+      <img [srcset]="user()!.avatar" [alt]="user()!.first_name" />
+
+      <div>
+        <h3>{{ user()?.first_name }} {{ user()?.last_name }}</h3>
+        <p>{{ user()?.email }}</p>
+      </div>
+    </section>
+
+    } @else {
+    <p>Loading info.</p>
+    }
+  `,
 })
 export default class UserComponent {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  // public user = signal<User| undefined>(undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
+
+   public titleLabel = computed(() => {
+     if (this.user()) {
+       return `User Info: ${this.user()?.first_name} ${
+         this.user()?.last_name
+       } `;
+     }
+
+     return 'User Info';
+   });
 }
